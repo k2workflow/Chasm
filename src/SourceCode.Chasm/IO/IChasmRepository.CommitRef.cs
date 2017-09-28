edbab9580250a4a22765005b128c4ca45a89515d
Grant Dickinson <grant@k2.com>
2017-09-28 14:02:08 -0700

    Rename repo to branch

--- src/SourceCode.Chasm/IO/IChasmRepository.CommitRef.cs
+++ src/SourceCode.Chasm/IO/IChasmRepository.CommitRef.cs
@@ -7,9 +7,9 @@ namespace SourceCode.Chasm.IO
     {
         #region Read
 
-        CommitId ReadCommitRef(string repo, string name);
+        CommitId ReadCommitRef(string branch, string name);
 
-        ValueTask<CommitId> ReadCommitRefAsync(string repo, string name, CancellationToken cancellationToken);
+        ValueTask<CommitId> ReadCommitRefAsync(string branch, string name, CancellationToken cancellationToken);
 
         #endregion
 
@@ -19,26 +19,26 @@ namespace SourceCode.Chasm.IO
         /// Write a <see cref="CommitRef"/> to the repository using the provided values.
         /// Note that the underlying store should use pessimistic concurrency control to prevent data loss.
         /// </summary>
-        /// <param name="repo">The repository name.</param>
+        /// <param name="branch">The repository name.</param>
         /// <param name="name">The name of the <see cref="CommitRef"/></param>
         /// <param name="previousCommitId">The previous <see cref="CommitId"/> that the caller used for reading.</param>
         /// <param name="newCommitId">The new <see cref="CommitId"/> that represents the content being written.</param>
         /// <exception cref="ChasmConcurrencyException">Thrown when a concurrency exception is detected.</exception>
         /// <returns></returns>
-        void WriteCommitRef(CommitId? previousCommitId, string repo, string name, CommitId newCommitId);
+        void WriteCommitRef(CommitId? previousCommitId, string branch, string name, CommitId newCommitId);
 
         /// <summary>
         /// Write a <see cref="CommitRef"/> to the repository using the provided values.
         /// Note that the underlying store should use pessimistic concurrency control to prevent data loss.
         /// </summary>
-        /// <param name="repo">The repository name.</param>
+        /// <param name="branch">The repository name.</param>
         /// <param name="name">The name of the <see cref="CommitRef"/></param>
         /// <param name="previousCommitId">The previous <see cref="CommitId"/> that the caller used for reading.</param>
         /// <param name="newCommitId">The new <see cref="CommitId"/> that represents the content being written.</param>
         /// <param name="cancellationToken">Allows the <see cref="Task"/> to be cancelled.</param>
         /// <exception cref="ChasmConcurrencyException">Thrown when a concurrency exception is detected.</exception>
         /// <returns></returns>
-        Task WriteCommitRefAsync(CommitId? previousCommitId, string repo, string name, CommitId newCommitId, CancellationToken cancellationToken);
+        Task WriteCommitRefAsync(CommitId? previousCommitId, string branch, string name, CommitId newCommitId, CancellationToken cancellationToken);
 
         #endregion
     }
